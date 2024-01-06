00cab21f4e9a12087101b4dc44bdbbb2bc4de3e7
Urban Ekar <ekar.urban@gmail.com>
2024-01-06 15:00:26 +0100

    Learned and used the class method inside JS

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let hours = 0; 
-let minutes = 0;
-let seconds = 0;
-let tens = 0; 
 let startBtn = document.querySelector('#start');
 let stopBtn = document.querySelector('#stop');
 let resetBtn = document.querySelector('#reset');
@@ -11,74 +7,138 @@ let appendMinutes = document.querySelector('#minutes');
 let appendSeconds = document.querySelector('#seconds');
 let appendTens = document.querySelector('#tens');
 
-let timerInterval;
-let startTime, elapsedTime = 0;
+
+window.addEventListener("scroll", function() {scrolling()})
+
+let anchor = document.querySelectorAll(".anchor")
+
+function scrolling() {
+	if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+		whenScrolled()
+	} else {
+		anchor.forEach((element) => element.style.display = "none");
+	}
+}
+
+function whenScrolled() {
+	anchor.forEach((element) => element.style.display = "block");
+	console.log("You scrolled")
+}
+
+
+
+
+/*
+function scrolling() {
+	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		whenScrolling()
+	} else {
+		whenNotScrolling()
+	}
+}
+
+function whenScrolling() {
+	document.querySelector(".anchor").style.display = "block";
+	document.querySelector(".anchor2").style.display = "block"
+	document.querySelector(".anchor3").style.display = "block"
+	console.log("You scrolled")
+}
+function whenNotScrolling() {
+	document.querySelector(".anchor").style.display = "none";
+		document.querySelector(".anchor2").style.display = "none";
+		document.querySelector(".anchor3").style.display = "none";
+		//ne vem zakva to noce delat: document.querySelector(".scoreboard").style.margin-bottom = "2000px"
+}*/
+
+
+class Timer {
+	constructor(hoursElement, minutesElement, secondsElement, tensElement) {
+		this.hoursElement = hoursElement;
+		this.minutesElement = minutesElement;
+		this.secondsElement = secondsElement
+		this.tensElement = tensElement;
+
+		this.timerInterval;
+		this.startTime
+		this.elapsedTime = 0;
+
+		this.updateTimer = this.updateTimer.bind(this);
+	}
+
+	start() {
+		this.startTime = performance.now() - this.elapsedTime;
+		if(!this.timerInterval)
+			this.timerInterval = setInterval(this.updateTimer, 10);
+	}
+
+	stop() {
+		clearInterval(this.timerInterval);
+		this.timerInterval = null;
+	}
+
+	reset() {
+		clearInterval(this.timerInterval);
+		this.timerInterval = null;
+		this.elapsedTime = 0;
+		this.startTime = performance.now();
+		this.updateTimer();
+	}
+
+	toggle() {
+		if(this.timerInterval) {
+			this.stop();
+		} else {
+			this.start();
+		}
+	}
+
+	updateTimer() {
+		this.elapsedTime = performance.now() - this.startTime;
+		console.log(this);
+		this.displayTime(this.elapsedTime);
+	}
+
+	displayTime(elapsedTime) {
+		
+		let tens = Math.floor(elapsedTime / 10 % 100);
+		let seconds = Math.floor(elapsedTime / 10 / 100 % 60);
+		let minutes = Math.floor((elapsedTime / 10 / 100 / 60) % 60);
+		let hours = Math.floor((elapsedTime / 10 / 100 / 60 / 60));
+		
+		this.tensElement.innerText = (tens <= 9 ? "0": "") + tens;
+		this.secondsElement.innerText = (seconds <= 9 ? "0": "") + seconds;
+		this.minutesElement.innerText = (minutes <= 9 ? "0": "") + minutes;
+		this.hoursElement.innerText = (hours <= 9 ? "0": "") + hours;
+	}
+}
+
+const timer = new Timer(appendHours, appendMinutes, appendSeconds, appendTens);
 
 window.onload = function() {
-    //this prevents the site scrolling when pressing space button
-    window.onkeydown = function(e) { 
-        return !(e.keyCode == 32);
-    };
 
-    //checking what key you pressed and starting,stopping,reseting
-    
-    document.body.addEventListener('keydown', (ev) => {
-        if(ev.key == " ") {
-            startTime = performance.now() - elapsedTime;
-            if(!timerInterval)
-                timerInterval = setInterval(updateTimer, 10);
-        }else if(ev.key == "Shift") {
-            clearInterval(timerInterval);
-            timerInterval = null;
-        }else if(ev.key == "Control") {
-            clearInterval(timerInterval);
-            timerInterval = null;
-            elapsedTime = 0;
-            startTime = performance.now();
-            hours = 0;
-            updateTimer();
-        }return
-    })
+	//this prevents the site scrolling when pressing space button
+	window.onkeydown = function(e) { 
+		return !(e.keyCode == 32);
+	};
+	//checking what key you pressed and then executing the code
+	document.body.addEventListener('keydown', (ev) => {
+		if(ev.key == " ") {
+			timer.toggle();
+		} else if(ev.key == "Escape") {
+			timer.reset()
+		}
+	})
 
-    startBtn.onclick = () => {
-        startTime = performance.now() - elapsedTime;
-        if(!timerInterval)
-            timerInterval = setInterval(updateTimer, 10);
-    }
+	startBtn.onclick = () => {
+		timer.start();
+	}
 
-    stopBtn.onclick = () => {
-        clearInterval(timerInterval);
-        timerInterval = null;
-    }
-    //when shift is pressed down, the reset button starts working
+	stopBtn.onclick = () => {
+		timer.stop();
+	}
 
-    resetBtn.onclick = () => {
-        clearInterval(timerInterval);
-        timerInterval = null;
-        elapsedTime = 0;
-        startTime = performance.now();
-        hours = 0;
-        updateTimer();
-    }
-
-
-    function updateTimer() {
-        elapsedTime = performance.now() - startTime;
-        
-        displayTime(elapsedTime);
-    }
-
-    function displayTime(elapsedTime) {
-        
-        tens = Math.floor(elapsedTime / 10 % 100);
-        seconds = Math.floor(elapsedTime / 10 / 100 % 60);
-        minutes = Math.floor((elapsedTime / 10 / 100 / 60) % 60);
-        hours = Math.floor((elapsedTime / 10 / 100 / 60 / 60));
-        
-        appendTens.innerText = (tens <= 9 ? "0": "") + tens;
-        appendSeconds.innerText = (seconds <= 9 ? "0": "") + seconds;
-        appendMinutes.innerText = (minutes <= 9 ? "0": "") + minutes;
-        appendHours.innerText = (hours <= 9 ? "0": "") + hours;
-    }
+	resetBtn.onclick = () => {
+		timer.reset();
+	}
 
 }
